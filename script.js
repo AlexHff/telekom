997a1250f4d9bbc176452a19ba8ee6606f625e16
Alexander Hoffmann <alexander@hoffmann.ai>
2021-07-20 16:23:07 +0200

    arrays and strings

--- script.js
+++ script.js
@@ -29,3 +29,53 @@ console.log(concat("Hello ", "World")); // OK
 console.log(concat("Hello", " World")); // OK
 console.log(concat("Hello ", " World")); // OK
 console.log(concat("Hello ", "     World")); // NOT OK
+
+for (var i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+var i = 0;
+
+while (i < 10) {
+  console.log(i);
+  i++;
+}
+
+var arr = [1, 7, 3, 5];
+var sum = 0;
+var max = Number.NEGATIVE_INFINITY;
+var min = Number.POSITIVE_INFINITY;
+var mean = 0;
+
+for (var i = 0; i < arr.length; i++) {
+  sum = sum + arr[i];
+  if (max < arr[i]) {
+    max = arr[i];
+  }
+  if (min > arr[i]) {
+    min = arr[i];
+  }
+}
+
+if (arr.length !== 0) {
+  mean = sum / arr.length;
+}
+
+var a = [
+  ["2", "5", "6"],
+  ["2", "5", "6"],
+  ["2", "5", "6"],
+];
+
+//        j j j
+//        = = =
+//        0 1 2
+// i = 0  a b c
+// i = 1  d e f
+// i = 2  g h i
+
+for (var i = 0; i < a.length; i++) {
+  for (var j = 0; j < a[i].length; j++) {
+    console.log(a[i][j]);
+  }
+}
